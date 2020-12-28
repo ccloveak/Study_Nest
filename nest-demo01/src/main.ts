a2a9b85cfa1cd53840668220e703e9e371a75de3
ccloveak <superstarzsw@gmail.com>
2020-12-29 03:05:21 +0900

    修复报错，格式化代码结构，调整vscode设置，开启自动格式化代码

--- nest-demo01/src/main.ts
+++ nest-demo01/src/main.ts
@@ -1,8 +1,8 @@
 import { AppModule } from './app.module';
 import { NestFactory } from '@nestjs/core';
 
-const bootstrap = async() => {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
-}
+const bootstrap = async () => {
+    const app = await NestFactory.create(AppModule);
+    await app.listen(3000);
+};
 bootstrap();

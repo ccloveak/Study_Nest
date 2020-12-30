e07a8dadc992d50c4d6cb7ede83920084accdce8
ccloveak <superstarzsw@gmail.com>
2020-12-31 01:42:37 +0900

    nest实现简单的curd

--- README.md
+++ README.md
@@ -1,10 +1,17 @@
 # Study_Nest
-又是一个新坑，主要是用node给前端提供api，安全方面以及其他的日后慢慢加
 
+又是一个新坑，主要是用 node 给前端提供 api，安全方面以及其他的日后慢慢加
 
 # 流程
 
-```bash
+1. 使用脚手架搭建框架
+2. 增加拼写检查
+3. 关联 mongondb
 
-nest new nest_demo01
-``` 
+```
+yanr add mongoose @nestjs/mongoose
+
+```
+
+    * 用docker起一个mongodb
+4. 进行user的curd
--- nest-demo01/src/app.module.ts
+++ nest-demo01/src/app.module.ts
@@ -1,9 +1,11 @@
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
+import { UserModule } from './server/user/user.module';
 
 @Module({
-    imports: [],
+    imports: [MongooseModule.forRoot('mongodb://localhost:27017/demo01'), UserModule],
     controllers: [AppController],
     providers: [AppService],
 })
